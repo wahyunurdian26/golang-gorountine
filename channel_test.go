b17aa2ee0916dd9cf4b15cf78c2bfe0694d9d48d
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-03-05 14:00:21 +0700

    channel parameter

--- channel_test.go
+++ channel_test.go
@@ -19,3 +19,43 @@ data := <-channel
 fmt.Println(data)
 time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string)  {
+	time.Sleep(2* time.Second)
+	channel <- "Wahyu Nurdian"
+}
+
+func TestChannelAsParameter(t * testing.T)  {
+	channel := make(chan string)
+defer close (channel)
+go GiveMeResponse(channel)
+
+data := <-channel
+fmt.Println(data)
+time.Sleep(5 * time.Second)
+
+}
+
+
+func OnlyIn( channel chan<- string)  {
+	time.Sleep(2 * time.Second)
+	channel <- "Wahyu Nurdian"
+
+	fmt.Println("Selesai mengirim data ke channel")
+}
+
+func OnlyOut( channel <-chan string)  {
+	data := <-channel
+fmt.Println(data)
+
+time.Sleep(5 * time.Second)
+}
+
+func TestInOutChannel( t *testing.T)  {
+	channel := make(chan string)
+defer close (channel)
+go OnlyIn(channel)
+go OnlyOut(channel)
+
+time.Sleep(5 * time.Second)
+}
