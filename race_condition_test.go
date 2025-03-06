c92369b6552599c642d036c73174a07b8ca7cb99
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-03-06 20:30:32 +0700

    race condition

---
+++ race_condition_test.go
@@ -0,0 +1,21 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRaceCondition(t *testing.T) {
+	var x = 0
+
+	for i := 1; i < 1000; i++ {
+		go func() {
+			for j := 1; j < 100; j++ {
+				x = x + 1
+			}
+		}()
+	}
+	time.Sleep(7 * time.Second)
+	fmt.Println("counter =", x)
+}
