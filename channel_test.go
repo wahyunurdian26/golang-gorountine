160ee8f9650310b5a0ab91f2800031cd949d1461
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-03-06 20:23:54 +0700

    default select

--- channel_test.go
+++ channel_test.go
@@ -120,3 +120,32 @@ func TestSeelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelect(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse((channel1))
+	go GiveMeResponse((channel2))
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
