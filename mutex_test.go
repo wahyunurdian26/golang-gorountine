624c340916adb5077dea14a545cbef1e3f579fb0
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-03-06 21:28:07 +0700

    RW Mutex

--- mutex_test.go
+++ mutex_test.go
@@ -10,7 +10,7 @@ import (
 func TestMutex(t *testing.T) {
 	var x = 0
 	var mutex sync.Mutex // mutex untuk menghindari race condition variavel yg di sharing
-
+	//lebih aman namun sedikit lambat
 	for i := 1; i <= 1000; i++ {
 		go func() {
 			for j := 1; j <= 100; j++ {
@@ -22,4 +22,38 @@ func TestMutex(t *testing.T) {
 	}
 	time.Sleep(5 * time.Second)
 	fmt.Println("counter =", x)
+}
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
 }
